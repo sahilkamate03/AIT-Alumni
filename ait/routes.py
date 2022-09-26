b3acf906a94e9d9ab96db566052826fa7a410b8e
SahilKamate <sahilkamate03@gmail.com>
2022-09-27 00:08:37 +0530

    small changes

--- ait/routes.py
+++ ait/routes.py
@@ -2,13 +2,16 @@ from flask import  render_template, url_for, redirect, jsonify
 
 import json
 
-from ait import app
+from ait import app, db
+from ait.forms import PostForm
 from ait.models import User, Post
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('home.html', title = 'Home')
+    posts = Post.query.all()
+    return render_template('home.html', title = 'Home',posts = posts)
 
 @app.route('/login')
 def login():
@@ -18,10 +21,21 @@ def login():
 def register():
     return render_template('./auth_page/pages-register.html', title = 'Register')
 
+
 @app.route('/get_post')
 def get_post():
-    view_post = render_template(
-        './post/view_post.html')
+    view_post = render_template('./post/view_post.html')
 
     data = {'remain': view_post}
     return jsonify(data)
+
+
+@app.route('/new_post', methods= ['GET','POST'])
+def new_post():
+    create_post = PostForm()
+    if create_post.validate_on_submit():
+        post = Post(title = create_post.title.data, content = create_post.content.data, author = 1)
+        db.session.add()
+        db.session.commit()
+        return redirect('home.html')
+    return render_template('new_post.html', create_post = create_post)
